b95dfd599b5a15d485dc7d980116c1128649632b
mateuszklos37@gmail.com <mateuszklos37@gmail.com>
2019-08-07 22:06:13 +0200

    First testing version

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,38 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.user.SimpleUser;
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
     public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        //first test
+        SimpleUser simpleUser = new SimpleUser("Example");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("Example")){
+            System.out.println("Test ok");
+        }
+        else{
+            System.out.println("Wrong name");
+        }
+        //second test
+        Calculator calculator1 = new Calculator();
+
+        int addingRes = calculator1.add(3, 2);
+        if (addingRes == calculator1.addingResult) {
+            System.out.println("Adding test ok");
+        }
+        else{
+            System.out.println("Adding test failed: wrong result");
+        }
+
+        int subtactingRes = calculator1.subtract(3, 2);
+        if (subtactingRes == calculator1.addingResult) {
+            System.out.println("Subtracting test ok");
+        }
+        else{
+            System.out.println("Subtracting test failed: wrong result");
+        }
     }
 }
---
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -0,0 +1,14 @@
+package com.kodilla.testing.calculator;
+
+public class Calculator {
+    public int addingResult;
+    public int subtractingResult;
+
+    public int add (int a, int b){
+        return addingResult;
+    }
+
+    public int subtract(int a, int b){
+        return subtractingResult;
+    }
+}
