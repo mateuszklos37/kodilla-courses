ab4eddf9eeb8a39c0559f363ff337bba025b8ea3
mateuszklos37@gmail.com <mateuszklos37@gmail.com>
2019-08-25 17:07:34 +0200

    Excercise 8.2

--- kodilla - exceptionn/src/main/java/main.java
+++
@@ -1,7 +0,0 @@
-public class main {
-    public static void main(String[] args) {
-////        FileReader fileReader = new FileReader();
-////        fileReader.readFile();
-        System.out.println("Hello");
-    }
-}
