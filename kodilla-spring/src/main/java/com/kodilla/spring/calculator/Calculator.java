f32cecb38f0d59866f130770446117a2ad6ea409
mateuszklos37@gmail.com <mateuszklos37@gmail.com>
2019-10-07 21:20:24 +0200

    Update

--- kodilla-spring/src/main/java/com/kodilla/spring/calculator/Calculator.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/calculator/Calculator.java
@@ -23,12 +23,11 @@ public class Calculator {
         display.displayVal(result);
         return result;
     }
-    public double div(double a, double b){
-        double result = a/b;
-        if(b==0){
-            b=1;
-            System.out.println("b changed automatically to 1 - do not divide by 0! Never!");
+    public double div(double a, double b) throws ArithmeticException{
+        if (b==0){
+            throw new ArithmeticException();
         }
+        double result = a / b;
         display.displayVal(result);
         return result;
     }
--- kodilla-spring/src/test/java/com/kodilla/spring/calculator/CalculatorTestSuite.java
+++ kodilla-spring/src/test/java/com/kodilla/spring/calculator/CalculatorTestSuite.java
@@ -1,5 +1,6 @@
 package com.kodilla.spring.calculator;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,20 +11,52 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class CalculatorTestSuite {
+    private static int counter= 0;
+    @Before
+    public void before(){
+        counter ++;
+        System.out.println("Every single test starts like this. The number of test is: " + counter);
+    }
     @Test
-    public void testCalculator(){
+    public void testAddMethod() {
         //Given
         ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring");
         Calculator calculator = context.getBean(Calculator.class);
         //When
         double resultOfAdding = calculator.add(4.7, 2.3);
-        double resultOfSubtracting = calculator.sub(4.7, 2.3);
-        double resultOfMultiplying = calculator.mul(4.7, 2.3);
-        double resultOfDivision = calculator.div(4.7, 2.3);
         //Then
         Assert.assertEquals(7.0, resultOfAdding, 0.01);
+    }
+    @Test
+    public void testSubtractMethod() {
+        //Given
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring");
+        Calculator calculator = context.getBean(Calculator.class);
+        double resultOfSubtracting = calculator.sub(4.7, 2.3);
+        //Then
         Assert.assertEquals(2.4, resultOfSubtracting, 0.01);
+    }
+    @Test
+    public void testMultiplyMethod() {
+        //Given
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring");
+        Calculator calculator = context.getBean(Calculator.class);
+        double resultOfMultiplying = calculator.mul(4.7, 2.3);
+        //Then
         Assert.assertEquals(10.81, resultOfMultiplying, 0.01);
-        Assert.assertEquals(2.04, resultOfDivision, 0.02);
+    }
+    @Test
+    public void testDivideMethod() {
+        //Given
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring");
+        Calculator calculator = context.getBean(Calculator.class);
+        try {
+            double resultOfDivision = calculator.div(4.7, 2.3);
+            //Then
+            Assert.assertEquals(2.04, resultOfDivision, 0.02);
+        }
+        catch (ArithmeticException e){
+            System.out.println("Do not ever divide by 0!");
+        }
     }
 }
