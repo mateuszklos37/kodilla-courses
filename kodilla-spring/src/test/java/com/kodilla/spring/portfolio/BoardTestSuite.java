62dc1e421df90c6d02a1591d1bc37e660683c79c
mateuszklos37@gmail.com <mateuszklos37@gmail.com>
2019-10-10 08:12:02 +0200

    14.4

--- kodilla-spring/src/test/java/com/kodilla/spring/portfolio/BoardTestSuite.java
+++ kodilla-spring/src/test/java/com/kodilla/spring/portfolio/BoardTestSuite.java
@@ -1,26 +1,41 @@
 package com.kodilla.spring.portfolio;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class BoardTestSuite {
-    @Test
-    public void testTaskAdd(){
+    Board board;
+    @Before
+    public void before(){
         //Given
         ApplicationContext context = new AnnotationConfigApplicationContext(BoardConfig.class);
-        Board board = context.getBean(Board.class);
+        this.board = context.getBean(Board.class);
+    }
+
+    @Test
+    public void testToDoTaskAdd(){
         //When
         TaskList toDoList = board.getToDoList();
         toDoList.addTask("Move the Safety System components");
+        //Then
+        System.out.println("To do list: " + toDoList.getTask(0));
+    }
+    @Test
+    public void testInProgressTaskAdd(){
+        //When
         TaskList inProgress = board.getInProgressList();
         inProgress.addTask("Resolving Weldguide communication problem");
+        //Then
+        System.out.println("In progress list: " + inProgress.getTask(0));
+    }
+    @Test
+    public void testDoneTaskAdd(){
+        //When
         TaskList doneList = board.getDoneList();
         doneList.addTask("Crew training");
         //Then
-        System.out.println("To do list: " + toDoList.getTask(0) + "\n" + "In progress list: " + inProgress.getTask(0) + "\n" + "Done list: " + doneList.getTask(0));
+        System.out.println("Done list: " + doneList.getTask(0));
     }
 }
