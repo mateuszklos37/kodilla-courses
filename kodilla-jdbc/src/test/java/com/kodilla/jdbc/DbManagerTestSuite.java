643f3fb06a2a05d30f09a208cf2bcdba6e56735c
mateuszklos37@gmail.com <mateuszklos37@gmail.com>
2019-10-22 20:01:05 +0200

    16.6

--- kodilla-jdbc/src/test/java/com/kodilla/jdbc/DbManagerTestSuite.java
+++ kodilla-jdbc/src/test/java/com/kodilla/jdbc/DbManagerTestSuite.java
@@ -1,5 +1,57 @@
 package com.kodilla.jdbc;
+
 import org.junit.Assert;
+import org.junit.Test;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class DbManagerTestSuite {
+    @Test
+    public void testConnect() throws SQLException {
+        //Given
+        //When
+        DBManager dbManager = DBManager.getInstance();
+        //Then
+        Assert.assertNotNull(dbManager.getConnection());
+    }
+    @Test
+    public void testSelectUser() throws SQLException {
+        //Given
+        DBManager dbManager = DBManager.getInstance();
+        //When
+        String sqlQuery = "SELECT * FROM USERS";
+        Statement statement = dbManager.getConnection().createStatement();
+        ResultSet rs = statement.executeQuery(sqlQuery);
+        //Then
+        int counter = 0;
+        while (rs.next()){
+            System.out.println(rs.getInt("ID") + ", " + rs.getString("FIRSTNAME") + ", " + rs.getString("LASTNAME"));
+            counter ++;
+        }
+        rs.close();
+        statement.close();
+        Assert.assertEquals(5, counter);
+    }
+    @Test
+    public void testSelectUsersAndPosts() throws SQLException{
+        //Given
+        DBManager dbManager = DBManager.getInstance();
+        //When
+        String sqlQuery = "SELECT U.FIRSTNAME, U.LASTNAME, P.USER_ID, COUNT(*) AS POSTS_NUMBER\n" +
+                "FROM USERS U\n" +
+                "JOIN POSTS P ON U.ID=P.USER_ID\n" +
+                "GROUP BY USER_ID\n" +
+                "HAVING COUNT(*)>1;";
+        Statement statement = dbManager.getConnection().createStatement();
+        ResultSet rs = statement.executeQuery(sqlQuery);
+        //Then
+        int counter = 0;
+        while(rs.next()){
+            System.out.println(rs.getString("FIRSTNAME") + ", " + rs.getString("LASTNAME") + ", user id: " + rs.getInt("USER_ID") + ", posts quantity: " + rs.getInt("POSTS_NUMBER"));
+            counter ++;
+        }
+        Assert.assertEquals(2, counter);
+    }
 }
