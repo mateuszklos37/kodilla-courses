86aa8ec176748e79454c2ff461cd962d2958b8fc
mateuszklos37@gmail.com <mateuszklos37@gmail.com>
2019-11-08 17:17:53 +0100

    17.1

---
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/KodillaHibertnateApllication.java
@@ -0,0 +1,10 @@
+package com.kodilla.hibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+public class KodillaHibertnateApllication {
+    public static void main(String[] args) {
+        SpringApplication.run(KodillaHibertnateApllication.class, args);
+    }
+}
---
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/KodillaHibernateApplicationTest.java
@@ -0,0 +1,14 @@
+package com.kodilla.hibernate;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KodillaHibernateApplicationTest {
+
+    @Test
+    public void contextLoads() {
+    }
+}
