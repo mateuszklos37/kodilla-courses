b6db95f1045ed52f2c3895059271b5b27290af62
mateuszklos37@gmail.com <mateuszklos37@gmail.com>
2019-11-11 19:22:59 +0100

    Update

--- kodilla-hibernate/src/main/java/com/kodilla/hibernate/KodillaHibernateApplication.java
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/KodillaHibernateApplication.java
@@ -1,10 +1,10 @@
 package com.kodilla.hibernate;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-public class KodillaHibertnateApllication {
+@SpringBootApplication
+public class KodillaHibernateApplication {
     public static void main(String[] args) {
-        SpringApplication.run(KodillaHibertnateApllication.class, args);
+        SpringApplication.run(KodillaHibernateApplication.class, args);
     }
 }
--- kodilla-hibernate/src/test/java/com/kodilla/hibernate/KodillaHibernateApplicationTest.java
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/KodillaHibernateApplicationTest.java
@@ -1,4 +1,5 @@
 package com.kodilla.hibernate;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
